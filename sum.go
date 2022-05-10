858be3339883d9388ef01f1b0b26a3d167913954
tang20130807 <101041970+tang20130807@users.noreply.github.com>
2022-05-10 13:27:29 +0800

    Add files via upload

---
+++ sum.go
@@ -0,0 +1,11 @@
+package calc
+
+var logMessage = "[LOG]"
+var Version = "1.0"
+
+func internalSum(number int) int {
+	return number - 1
+}
+func Sum(number1 int, number2 int) int {
+	return number1 + number2
+}
